58d15f14c19b2a984175a28f2f7ef0d8199bef34
Aslak Johansen <aslakj@gmail.com>
2021-05-26 17:39:18 +0200

    Added empty project

---
+++ mqtt_mavg/lib/mqtt_mavg.ex
@@ -0,0 +1,18 @@
+defmodule MqttMavg do
+  @moduledoc """
+  Documentation for MqttMavg.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MqttMavg.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ mqtt_mavg/test/mqtt_mavg_test.exs
@@ -0,0 +1,8 @@
+defmodule MqttMavgTest do
+  use ExUnit.Case
+  doctest MqttMavg
+
+  test "greets the world" do
+    assert MqttMavg.hello() == :world
+  end
+end
